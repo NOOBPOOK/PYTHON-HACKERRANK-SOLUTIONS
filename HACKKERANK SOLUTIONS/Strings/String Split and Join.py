5e9cfa4faa1e23e03467aa99de2ee8a273cdd63d
Vedant Rajendra Shetye <97832138+NOOBPOOK@users.noreply.github.com>
2022-09-10 10:48:17 +0530

    yes

--- HACKKERANK SOLUTIONS/Strings/String Split and Join.py
+++
@@ -1,9 +0,0 @@
-def split_and_join(line):
-    # write your code here
-    a = line.split(" ")
-    return ('-'.join(a))
-
-if __name__ == '__main__':
-    line = input()
-    result = split_and_join(line)
-    print(result)
